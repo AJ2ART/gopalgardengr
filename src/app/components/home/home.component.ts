13f204c03d7b2665f3039ab2b0b098704819bc68
Yatin Singla <yatin.singla@cargomatrix.com>
2021-06-16 18:44:09 -0400

    added footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  totalValue = 300000;
+  valueReceived = 135000;
+  progress = '0%';
+
   constructor() { }
 
   ngOnInit() {
+    this.progress = `${(this.valueReceived * 100) / this.totalValue}%`;
   }
 
 }
